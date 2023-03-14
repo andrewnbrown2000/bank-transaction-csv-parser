7b3d2efd63907f8d70f330e760630680ee363bd3
ian-bird <ianukusa@gmail.com>
2023-03-14 14:34:29 -0500

    fix csvParser

--- csvParser.rb
+++ csvParser.rb
@@ -1,36 +1,39 @@
-def csv_parser file_path
-  csv_lines = File.read(file_path).split(/\n/).reject do |line| # read in the file and clean it up by removing blank lines and headers
-    line.empty? or line.start_with?('<')                            # and setting it equal to an array "csv_lines"
-  end
-  csv_items_arr = csv_lines.map do |line| # split the created array into a 2d array. Big array is all transactions, and small arrays
-    line.split(',')                         # represent a transaction and its data
-  end
-  csv_items_arr.each do |transaction| # remove 1st and 2nd element of each array, they are useless
-    transaction.slice!(1..2)
-  end
-end
+file_contents = File.read 'transactions.csv'
 
-transaction_data = csv_parser('C:\Users\Andrew\Desktop\budgetSheets\transactions.csv') # parse and clean csv, turn it into a 2D array
+header = []
+table = []
 
+file_contents = file_contents.split("\n").reject(&:empty?).join("\n")
 
-class Transaction
-  attr_reader :date, :withdrawal, :deposit, :vendor
-  def initialize(date, withdrawal, deposit, vendor)
-    @date = date
-    @withdrawal = withdrawal
-    @deposit = deposit
-    @vendor = vendor
-
+file_contents.each_line do |line|
+  if line.start_with? '<' # its a header
+    # remove the trailing \n, remove the <'s and >'s, split on commas.
+    # this gives an array of the column names
+    header = line.gsub("\n", '').gsub('<', '').gsub('>', '').split(',')
+  else
+    # this method will split on commas not in parentheses
+    table << line.split(/(?!\B"[^"]*),(?![^"]*"\B)/)
   end
 end
 
-transaction_data.each do |transaction|
-  transaction << Transaction.new(transaction[0], transaction[1], transaction[2], transaction[3])
-  transaction[3].slice!(/(?<=\d{6,7}\s+).*?(?=(\d|\s\s))/)
-  puts "#{transaction} + ***"
-  # transactions \\<< Transaction.send([:new] + transaction)
+# convert table from [row #][column #] to [row #][column name]
+table.collect! do |row|
+  column_names = header.each
+  hash = {}
+  row.each { |field| hash[column_names.next] = field }
+  hash
 end
 
+# for each row
+table.collect! do |row|
+  # if additional info on this row has terminal then a number then a date
+  if row['Additional Info'].match?(/TERMINAL\s\d+\s(.*?)\s+\d+-/)
+    # only take the values after (terminal number) and before the date
+    row['Additional Info'] = row['Additional Info'].match(/TERMINAL\s\d+\s(.*?)\s+\d+-/)[1]
+    # only keep words that don't have digits, replace all spaces of greater than one space with a single space
+    row['Additional Info'] = row['Additional Info'].gsub(/\b\w*\d\w*\b/, '').gsub(/\s\s+/, ' ')
+  end
+  row
+end
 
-# will need to do a regex here to combine vendors. might further consolidate into categories (i.e. food, gas, etc) if possible?
-# puts 'combine the transaction data with the preexisting vendor data' if vendors.include?(vendor)
+puts table
