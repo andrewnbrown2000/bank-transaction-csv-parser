54593e3795f4c3da7fc6291d09f405f258c2d606
andrewnbrown2000 <andrewbrown.n2000@gmail.com>
2023-03-16 22:29:54 -0500

    Trim vendor string and fixed nil vendors in csv file

--- csvParser.rb
+++ csvParser.rb
@@ -10,27 +10,34 @@ def csv_parser file_path
   end
 end
 
-transaction_data = csv_parser('C:\Users\Andrew\Desktop\budgetSheets\transactions.csv') # parse and clean csv, turn it into a 2D array
+transaction_data = csv_parser('C:\Users\Andrew\Desktop\budgetSheets\2022sheets\2022transactions.csv') # parse and clean csv, turn it into a 2D array
 
 
 class Transaction
-  attr_reader :date, :withdrawal, :deposit, :vendor
+  attr_reader :date, :withdrawal, :deposit #r only
+  attr_accessor :vendor #r/w
   def initialize(date, withdrawal, deposit, vendor)
     @date = date
     @withdrawal = withdrawal
     @deposit = deposit
-    @vendor = vendor
-
+    @vendor = if vendor.nil? # protects against csv file containing nil vendors
+                'VENDOR UNKNOWN'
+              else
+                vendor
+              end
   end
 end
 
 transaction_data.each do |transaction|
-  transaction << Transaction.new(transaction[0], transaction[1], transaction[2], transaction[3])
-  transaction[3].slice!(/(?<=\d{6,7}\s+).*?(?=(\d|\s\s))/)
-  puts "#{transaction} + ***"
-  # transactions \\<< Transaction.send([:new] + transaction)
+  transaction = Transaction.new(transaction[0], transaction[1], transaction[2], transaction[3]) # load objects with respective data
+  if transaction.vendor.match?(/TERMINAL.*?\s{2}/) #if statements to trim the vendor strings
+    transaction.vendor = transaction.vendor.match(/TERMINAL.*?\s{2}/).to_s
+    if transaction.vendor.match?(/TERMINAL \d{6,7}\s+(.*)/)
+      transaction.vendor = transaction.vendor.match(/TERMINAL \d{6,7}\s+(.*)/)[1]
+    end
+  end
+  if transaction.vendor.match?(/TERMINAL [0-9]+ (.*?)\s+\b\w*\d/)
+    transaction.vendor = transaction.vendor.match(/TERMINAL [0-9]+ (.*?)\s+\b\w*\d/)[1]
+    puts transaction.vendor
+  end
 end
-
-
-# will need to do a regex here to combine vendors. might further consolidate into categories (i.e. food, gas, etc) if possible?
-# puts 'combine the transaction data with the preexisting vendor data' if vendors.include?(vendor)
